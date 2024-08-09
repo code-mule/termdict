5e610d151ccfce4cac12b97faecb9534cbdbffe5
bertrandterrier <i.stemmert@gmail.com>
2024-08-09 22:59:08 +0200

    Worked on create_term_tbl()

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from ios import first_time
 from com import get_cl_args
 import pandas as pd
 import sys
+from tabulate import tabulate
 
 # ================================================== #
 
@@ -31,13 +32,39 @@ class TermDict:
     def __init__(
             self,
             csv:str,
-            data:str,
+            data_dir:str,
             tbl_style:str,
-            new:bool
+            new:bool,
+            highlight_col:dict,
+            highlight_dict
             ) -> int:
+        # Colors
+        self.MAGENTA = '\033[5m'
+        self.CYAN = '\033[6m'
+        self.YELLOW = '\033[3m'
+
+        # Data
         self.df = pd.read_csv(csv)
         self.cols = self.df.columns
+        self.hl_cols = highlight_col
+        self.hl_dict = highlight_dict
 
+        # Create table from pandas DataFrame
+        self.tbl:str = self.create_term_tbl(tbl_style,highlight_cols)
+
+    def create_term_tbl(self,style) -> str:
+        tabel:str = tabulate(tabular_data=self.df,
+                                headers='keys',
+                                TableFormat=style
+                                )
+        tmp_df:pd.DataFrame = self.df.copy()
+
+        col_row = {
+            0 : [],
+            1 : [],
+            2 : []
+        }
+        
 
 def main():
     # Create configuration directory if not existing
